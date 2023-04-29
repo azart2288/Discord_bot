1d621504e868efaec8a1160324649a4e4335847e
azart2288 <96446652+azart2288@users.noreply.github.com>
2023-04-29 17:28:49 +0300

    Add files via upload

---
+++ second.py
@@ -0,0 +1,111 @@
+import discord
+from discord.ext import commands
+import json  
+import asyncio
+from pydub import AudioSegment
+from pydub.playback import play
+import wget
+import requests
+
+file = open('config.json' , 'r')
+config = json.load(file)
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(config["prefix"], intents=intents)
+
+@bot.command(name="ping")
+async def ping(ctx):
+    await ctx.send(f'{ctx.author.mention}нет')
+
+@bot.command(name="play")
+async def play(ctx, *args):
+    # Проверяем, что пользователь находится в голосовом канале
+    if ctx.author.voice is None:
+        await ctx.send("Вы не находитесь в голосовом канале.")
+        return
+    try:
+        await ctx.voice_client.disconnect()
+    except:
+        pass
+    
+
+    link = None
+    for arg in args:
+        if arg.startswith('-l='):
+            link = arg[3:]
+    
+    if link is None:
+        await ctx.send("Вы не указали ссылку на аудиофайл.")
+        return
+
+    print(link)
+    filename = link.split("/")[-1]
+    #print(filename)
+    r = requests.get(link, allow_redirects = True)
+    open(filename, "wb").write(r.content)
+ 
+
+    # Подключаемся к голосовому каналу пользователя
+    voice_channel = ctx.author.voice.channel
+    vc = await voice_channel.connect()
+
+    # Создаем AudioSegment объект и воспроизводим его
+    audio = AudioSegment.from_file(filename, format='mp3')
+    play(audio)
+
+    # Ждем, пока аудио не закончится
+    while vc.is_playing():
+        await asyncio.sleep(1)
+
+    # Отключаемся от голосового канала
+    await vc.disconnect()
+
+
+
+
+@bot.command(name="play_radio")
+async def play_radio(ctx, r_name):
+    # Проверяем, что пользователь находится в голосовом канале
+    if ctx.author.voice is None:
+        await ctx.send("Вы не находитесь в голосовом канале.")
+        return
+
+    try:
+        await ctx.voice_client.disconnect()
+    except:
+        pass
+
+    voice_channel = ctx.author.voice.channel
+    vc = await voice_channel.connect()
+
+    with open('r-list.json' ,encoding="utf-8") as f:
+        data = json.load(f)
+
+    if r_name in data:
+        audio = AudioSegment.from_file(data[r_name], format='mp3')
+        play(audio)
+
+        while vc.is_playing():
+            await asyncio.sleep(1)
+
+        # Отключаемся от голосового канала
+        await vc.disconnect()
+    else:
+        await ctx.send(f"Радиостанция {r_name} не найдена в списке.")
+        stations = "\n".join(data.keys())
+        await ctx.send(f"Список всех радиостанций:\n{stations}")
+        await vc.disconnect()
+
+
+@bot.command(name="stop")
+async def stop(ctx):
+
+    if ctx.voice_client is None:
+        await ctx.send("Бот не находится в голосовом канале.")
+        return
+    await ctx.voice_client.disconnect()
+
+
+bot.run(config['token'])
+
---
+++ third.py
@@ -0,0 +1,19 @@
+x = int(input("Введіть номер тижня"))
+
+if x == 1:
+    print("Понеділок")
+elif x == 2:
+    print("Вівторок")
+elif x == 3:
+    print("Середа")
+elif x == 4:
+    print("Четвер")
+elif x == 5:
+    print("П'ятниця")
+elif x == 6:
+    print("Субота")
+elif x == 7:
+    print("Неділя")
+
+
+
